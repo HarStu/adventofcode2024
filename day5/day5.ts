b19ada4d4d4c8db2f7bf25c175c28d2444eeba37
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-26 18:01:57 -0400

    Finish day 5 part 1

--- day5/day5.ts
+++ day5/day5.ts
@@ -16,7 +16,7 @@ const legalPages = new Set(xRulePages?.filter((page) => yRulePages?.includes(pag
 // Build a map of unlocks
 const mainUnlockMap = new Map<string, string[]>()
 for (let rule of rules!) {
-  mainUnlockMap.set(rule[0]!, (mainUnlockMap.get(rule[0]!) || []).concat(rule[1]!))
+  mainUnlockMap.set(rule[1]!, (mainUnlockMap.get(rule[1]!) || []).concat(rule[0]!))
 }
 
 console.log(mainUnlockMap)
@@ -24,19 +24,49 @@ console.log(mainUnlockMap)
 function part1(rules: string[][], production: string[][]) {
   let total = 0
   for (const row of production) {
-    // Build the ruleSet, lockedSet, unlockedSet, and unlockMap
     const unlockMap = new Map<string, string[]>()
-    const lockedNums = new Set()
-    for (const rule of rules) {
-      if (row.includes(rule[0]!) && row.includes(rule[1]!)) {
-        unlockMap.set(rule[0]!, (unlockMap.get(rule[0]!) || []).concat(rule[1]!))
-        lockedNums.add(rule[1]!)
+    for (const num of row) {
+      if (mainUnlockMap.get(num)) {
+        unlockMap.set(num, mainUnlockMap.get(num)!.filter(val => row.includes(val)))
       }
     }
-    console.log(`For production: ${row}, unlockmap is:`)
-    console.log(unlockMap)
-    console.log(lockedNums)
 
+    console.log(unlockMap)
+    console.log(row)
+
+    let midVal = Number(row[Math.floor(row.length / 2)])
+    const seen = new Set()
+    for (let num of row) {
+      // look up num on the unlockMap
+      // if all of the values associated with it are in 'seen',
+      // or it's not an entry in the map at all, 
+      // add it to 'seen'
+      // otherwise, kill the midVal
+      const preReqs = unlockMap.get(num)
+      console.log(`prereqs of ${num} are: ${preReqs}`)
+      if (!preReqs || preReqs.length === 0) {
+        seen.add(num)
+        console.log(`empty prereqs for ${num}, so we add it to seen. seen is now ${seen}`)
+      } else {
+        // check if preReqs of num is a subset of seen
+        // if it is, continue
+        // if it's not, set midVal to zero
+        if ([...preReqs].every(val => seen.has(val))) {
+          seen.add(num)
+        } else {
+          midVal = 0
+        }
+      }
+    }
+    total += midVal
+    console.log('\n\n\n\n')
+  }
+  return total
+}
+
+console.log(part1(rules!, production!))
+
+/*
     let midVal = Number(row[Math.floor(row.length / 2)])
     for (let num of row) {
       if (lockedNums.has(num)) {
@@ -48,8 +78,12 @@ function part1(rules: string[][], production: string[][]) {
       }
     }
     total += midVal
-  }
-  return total
-}
+*/
 
-console.log(part1(rules!, production!))
+/*
+      if (unlockMap.has(num)) {
+        for (const val of unlockMap.get(num)!) {
+          lockedNums.delete(val)
+        }
+      }
+        */
