112c91bef46e00ad80f2961cfa5c6194ae07e698
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-26 15:29:11 -0400

    build checkXmas

--- day4/day4.ts
+++ day4/day4.ts
@@ -4,6 +4,22 @@ const input = readFileSync('./day4_input.txt', "utf8")
 
 const xword = input.split('\n').map(row => row.split(''))
 
+function checkXMas(xword: string[][]) {
+  let matches = 0
+  const validStrs = ['MMSS', 'SSMM', 'MSMS', 'SMSM']
+  for (let x = 1; x < xword[0]!.length - 1; x++) {
+    for (let y = 1; y < xword.length - 1; y++) {
+      // build an 'observation string
+      const obvStr = "".concat(xword[y - 1]![x - 1]!).concat(xword[y - 1]![x + 1]!).concat(xword[y + 1]![x - 1]!).concat(xword[y + 1]![x + 1]!)
+      console.log(`AT [${y}][${x}], we have observation string: ${obvStr}`)
+      if (validStrs.includes(obvStr)) {
+        matches++
+      }
+    }
+  }
+  return matches
+}
+
 function checkXWord(xword: string[][], target: string) {
   const targetArr = genTarget(target)
   let matches = 0
