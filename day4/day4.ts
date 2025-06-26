2ab4eb768733ed71ef412152176613eb3da31e68
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-26 14:08:04 -0400

    Fix bug

--- day4/day4.ts
+++ day4/day4.ts
@@ -4,13 +4,25 @@ const input = readFileSync('./day4_input.txt', "utf8")
 
 const xword = input.split('\n').map(row => row.split(''))
 
-console.log(xword)
-
 function checkXWord(xword: string[][], target: string) {
   const targetArr = genTarget(target)
+  let matches = 0
+  // check both targets (forward and reversed)
   for (const target of targetArr) {
 
+    // check horizontal
+    for (const row of xword) {
+      //console.log(`ROW: ${row}`)
+      for (let i = 0; i <= row.length - target.length; i++) {
+        console.log(`SLICE ${i}: ${row.slice(i, i + target.length)} vs TARGET: ${target}`)
+        if (arrEq(target, row.slice(i, i + target.length))) {
+          matches++
+          console.log(`SLICE ${i}: ${row.slice(i, i + target.length)} vs TARGET: ${target} AT ${i}`)
+        }
+      }
+    }
   }
+  return matches;
 }
 
 function genTarget(s: string): string[][] {
@@ -23,4 +35,8 @@ function genTarget(s: string): string[][] {
   return [t1, t2]
 }
 
-console.log(genTarget('xmas'))
+function arrEq(arr1: string[], arr2: string[]): boolean {
+  return JSON.stringify(arr1) === JSON.stringify(arr2)
+}
+
+console.log(checkXWord(xword, 'xmas'))
