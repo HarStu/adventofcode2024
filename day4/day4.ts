3cd9d3ac4baef5550e1cdea22b3a9dbbb1ead25c
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-26 13:38:37 -0400

    Setup target construction from string

--- day4/day4.ts
+++ day4/day4.ts
@@ -5,3 +5,19 @@ const input = readFileSync('./day4_input.txt', "utf8")
 const xword = input.split('\n').map(row => row.split(''))
 
 console.log(xword)
+
+function checkXWord(xword: string[][], target: string) {
+  const targetArr = genTarget(target)
+  for (const target of targetArr) {
+
+  }
+}
+
+function genTarget(s: string): string[][] {
+  const t1 = []
+  for (const char of s) {
+    t1.push(char)
+  }
+  const t2 = t1.reverse()
+  return [t1, t2]
+}
