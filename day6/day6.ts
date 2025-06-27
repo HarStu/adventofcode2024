a19ad16653aac1652f255b385329a41e92766278
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-27 12:13:31 -0400

    Continue implementation

--- day6/day6.ts
+++ day6/day6.ts
@@ -14,7 +14,7 @@ type GuardState = {
 }
 
 type WorldState = {
-  moveHis: string[][][],
+  moveHis: Direction[][][],
   map: string[][],
   guard: GuardState,
   blockSpots: Set<string>
@@ -27,8 +27,9 @@ nextDir.set('v', '<')
 nextDir.set('<', '^')
 
 function runSim() {
-  const moveHis = []
+  const moveHis: Direction[][][] = []
   for (let y = 0; y < map.length; y++) {
+    moveHis[y] = []
     for (let x = 0; x < map[0].length; x++) {
       moveHis[y][x] = []
     }
@@ -79,10 +80,9 @@ function updateWorld(world: WorldState): WorldState {
   // Update the current position of the guard to 'X'
   world.map[world.guard.y][world.guard.x] = "X"
 
-  // Update the move history of the current tile to '
+  // Update the move history of the current tile
   world.moveHis[world.guard.y][world.guard.x].push(world.guard.dir)
 
-  // Get the direction the guard is going to move in next:
 
   let validMove = false
   while (!validMove) {
@@ -109,17 +109,17 @@ function updateWorld(world: WorldState): WorldState {
     if (nextMove[0] < 0 || nextMove[0] >= world.map[0].length || nextMove[1] < 0 || nextMove[1] >= world.map.length) {
       // if the nextMove is taking the guard out-of-limits, update the 
       // guard position and immediately return the world
-
-      // check -- if we did turn here, would be be repeating a direction in moveHis?
-      // if so, our "nextMove" is a valid blocker
-      if (world.moveHis[world.guard.y][world.guard.x].includes(nextDir.get(world.guard.dir))) {
-        world.blockSpots.add(`${nextMove[0]},${nextMove[1]}`)
-      }
-
       world.guard.y = nextMove[0]
       world.guard.x = nextMove[1]
       return world
     } else if (world.map[nextMove[0]][nextMove[1]] !== '#') {
+      // check -- if we did turn here, would be be repeating a direction in moveHis?
+      // if so, our "nextMove" is a valid blocker
+      console.log(`moveHis here at ${world.guard.y},${world.guard.x}: ${world.moveHis[world.guard.y][world.guard.x]}\n\tnextDir: ${nextDir.get(world.guard.dir)}`)
+      if (world.moveHis[world.guard.y][world.guard.x].includes(nextDir.get(world.guard.dir))) {
+        world.blockSpots.add(`${nextMove[0]},${nextMove[1]}`)
+      }
+
       // if the next move isn't blocked by a #, move is valid
       // update guard pos and set validMove to true to escape the
       // while loop
